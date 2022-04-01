9f0ab79e3b3c80045f4227dbfd25b6a37f99f0ee
Daniel <s0961102@gm.ncue.edu.tw>
2022-04-01 22:52:08 +0800

    contact完成

---
+++ js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "請輸入您的大名",
+                    minlength: "名字至少2個字元"
+                },
+                subject: {
+                    required: "請輸入您的主旨",
+                    minlength: "主旨至少4個字元"
+                },
+                number: {
+                    required: "請輸入您的手機號碼",
+                    minlength: "手機號碼至少5個字元"
+                },
+                email: {
+                    required: "請輸入您的信箱地址"
+                },
+                message: {
+                    required: "您需要寫些內容",
+                    minlength: "要寫多一點喔？"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
---
+++ js/sign.js
@@ -0,0 +1,76 @@
+
+$(document).ready(function ($) {
+    //for select
+    $.validator.addMethod("notEqualsto", function (value, element, arg) {
+        return arg != value;
+    }, "您尚未選擇!");
+
+    $("#form1").validate({
+        submitHandler: function (form) {
+            alert("success!");
+            form.submit();
+        },
+        rules: {
+            account: {
+                required: true,
+                minlength: 4,
+                maxlength: 10
+            },
+            password: {
+                required: true,
+                minlength: 6,
+                maxlength: 12
+            },
+
+        },
+        messages: {
+            account: {
+                required: "帳號為必填欄位",
+            },
+            password: {
+                required: "密碼為必填欄位",
+            },
+
+        }
+    });
+    $("#form2").validate({
+        submitHandler: function (form) {
+            alert("success!");
+            form.submit();
+        },
+        rules: {
+            account1: {
+                required: true,
+                minlength: 4,
+                maxlength: 10
+            },
+            mail1: {
+                required: true,
+                email: true
+            },
+
+            password1: {
+                required: true,
+                minlength: 6,
+                maxlength: 12
+            },
+            checkpassword1: {
+                required: true,
+                equalTo: "#password1"
+            },
+
+        },
+        messages: {
+            account1: {
+                required: "帳號為必填欄位",
+                minlength: "帳號最少要4個字",
+                maxlength: "帳號最長10個字"
+            },
+            checkpassword1: {
+                equalTo: "兩次密碼不相同",
+            },
+
+        }
+    });
+});
+
