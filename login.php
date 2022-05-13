b3789c4745bf25e948be00c5ebb0a463a969f1fd
Yunitrish006006 <yunthomas006@gmail.com>
2022-05-13 20:27:49 +0800

    帳戶資料讀取部分

--- login.php
+++ login.php
@@ -1,46 +1,53 @@
 <?php
-session_start();
-if (isset($_POST['user_id']) && isset($_POST['user_pwd'])) {
-//認證(Authentication):連線資料庫驗證使用者的帳號密碼是否正確
-//授權(Authorization):連線資料庫檢查使用者的身分別(會員、管理者....)
-if ($_POST['user_id'] == "member" && $_POST['user_pwd'] == "12345") {
-$_SESSION['user_id'] = $_POST['user_id'];
-$_SESSION['level'] = 2; //一般會員
-}
-
-if ($_POST['user_id'] == "admin" && $_POST['user_pwd'] == "12345") {
-$_SESSION['user_id'] = $_POST['user_id'];
-$_SESSION['level'] = 9; //管理者
-}
-
-}
-
-$content = <<<EOT
-    <div class="row">
-        <div class="col-md-4 col-md-offset-4">
-            <div class="login-panel panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">使用者登入</h3>
-                </div>
-                <div class="panel-body">
-                    <form id="form" role="form" name="form1" action="" method="POST">
-                        <fieldset>
-                        <div class="form-group">
-                            <input class="form-control" placeholder="帳號" name="user_id" type="text" required minlength="2" maxlength="20" autofocus>
-                        </div>
-                        <div class="form-group">
-                            <input class="form-control" placeholder="密碼" name="user_pwd" type="password" required minlength="4" maxlength="20" value="">
-                        </div>
-                        <!-- Change this to a button or input when using this as a form -->
-                        <button type="submit" id="login" class="btn btn-lg btn-success btn-block">登入</button>
-                        </fieldset>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-EOT;
-
-include "template.php";
-
+    if (session_status() === PHP_SESSION_NONE) session_start();
+    if (isset($_POST['account']) && isset($_POST['password'])) {
+        $host = 'localhost';
+        $user ='root';
+        $access = '';
+        $datagram = 'beehotel';
+        $link = mysqli_connect($host,$user,$access,$datagram);
+    
+        if ( !$link ) {
+            echo "連結錯誤代碼: ".mysqli_connect_errno()."<br>";//顯示錯誤代碼
+            echo "連結錯誤訊息: ".mysqli_connect_error()."<br>";//顯示錯誤訊息
+            exit();
+        }
+        else
+        {
+            mysqli_query($link, 'SET CHARACTER SET utf8');
+            mysqli_query($link, "SET collation_connection = 'utf8_unicode_ci'");
+    
+            // // 資料庫查詢(送出查詢的SQL指令)
+            if ($result = mysqli_query($link, "SELECT * FROM account")) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    //認證(Authentication):連線資料庫驗證使用者的帳號密碼是否正確
+                    //授權(Authorization):連線資料庫檢查使用者的身分別(會員、管理者....)
+                    if ($_POST['account'] == $row['account'] && $_POST['password'] == $row['password']) {
+                        $_SESSION['account'] = $row["account"];
+                        $_SESSION['level'] = $row["level"];
+                        $_SESSION['password'] = $row["password"];
+                        $_SESSION["email"] = $row["email"];
+                        switch ($_SESSION['level']) {
+                            case 99:
+                                header("Location:backend.php");
+                                break;
+                            case 1:
+                                header("Location:book.php");
+                                break;
+                            default:
+                                header("Location:index.php");
+                                break;
+                        }
+                    }
+                }
+                $num = mysqli_num_rows($result); //查詢結果筆數
+                mysqli_free_result($result); // 釋放佔用的記憶體
+            }
+            mysqli_close($link); // 關閉資料庫連結
+        }
+        include("index.php");
+    }
+    else {
+        header("Location:index.php");
+    }  
 ?>
