77fd51b2c5418268f70b644d5178b42c3e6bd1ac
Daniel <s0961102@gm.ncue.edu.tw>
2022-05-15 00:32:21 +0800

    資料庫更新

--- 資料庫.sql
+++ 資料庫.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
--- 主機： 127.0.0.1
--- 產生時間： 2022-05-14 16:10:08
--- 伺服器版本： 10.4.22-MariaDB
--- PHP 版本： 8.1.2
+-- 主機： localhost
+-- 產生時間： 2022 年 05 月 14 日 18:30
+-- 伺服器版本： 10.4.21-MariaDB
+-- PHP 版本： 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- 資料庫: `beehotel`
 --
-CREATE DATABASE IF NOT EXISTS `beehotel` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `beehotel`;
 
 -- --------------------------------------------------------
 
@@ -92,22 +90,48 @@ INSERT INTO `cart` (`id`, `account_id`, `pno`) VALUES
 --
 
 CREATE TABLE `comments` (
-  `id` int(10) NOT NULL,
-  `account_id` int(11) NOT NULL,
+  `id` varchar(10) NOT NULL,
+  `account` varchar(20) NOT NULL,
+  `name` varchar(10) NOT NULL,
   `date` date NOT NULL,
-  `picture_path` varchar(20) DEFAULT NULL,
-  `food_tag` tinyint(1) NOT NULL,
-  `service_tag` tinyint(1) NOT NULL,
-  `activity_tag` tinyint(1) NOT NULL,
-  `room_tag` tinyint(1) NOT NULL
+  `picture_path` varchar(50) DEFAULT NULL,
+  `tag` varchar(20) NOT NULL,
+  `subject` varchar(20) NOT NULL,
+  `comment` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- 傾印資料表的資料 `comments`
 --
 
-INSERT INTO `comments` (`id`, `account_id`, `date`, `picture_path`, `food_tag`, `service_tag`, `activity_tag`, `room_tag`) VALUES
-(0, 0, '2022-05-13', '', 0, 0, 0, 0);
+INSERT INTO `comments` (`id`, `account`, `name`, `date`, `picture_path`, `tag`, `subject`, `comment`) VALUES
+('1', 'Lin', '林小姐', '2022-05-01', 'images/blog/main-blog/m-blog-1.jpg', '食物,服務人員', '服務人員請不要氣餒', '好幾年公司尾牙都在此舉辦，今年當然也不例外，疫情當下在台灣還能大啖美食很幸福。今年有個小插曲，\r\n                                            服務員在送餐時（玉露海石斑）不小心湯汁濺灑在我衣服整隻袖子，感覺服務員很生澀應該是工讀生，\r\n                                            用餐過程到結束來了好幾位主管來跟我致歉，及詢問我需要什麼善後服務之類，還說要拿件衣服給我換，\r\n                                            或者把衣服送過來清洗都沒關係，我回沒關係不介意別責怪那位服務員，希望那位工讀生別往心裡去，\r\n                                            服務業辛苦了。'),
+('2', 'Yang', '楊先生', '2022-05-15', 'images/blog/main-blog/m-blog-8.jpg', '食物,服務人員,套房', '於房間設計與服務人員看法', '228連假期間入住12樓全新改裝的雅緻客房，房內裝潢走典雅木質系，空間寬敞\r\n                                            ，衛浴設施新穎，有免治馬桶，盥洗泡澡水量強勁且大，但室內燈光偏昏暗，\r\n                                            夜晚想在房內看書，只有書桌前上方的小燈可用，角落若能增加些立燈，應該會更好些\r\n                                            ，當晚還遇到11點半樓上房客一直在搬家具的聲音，幸好反應給櫃檯人員後就改善了。\r\n                                            隔日的早餐是採半自助式的，除了可無限點用限定的6種套餐外，再加上自助吧的沙拉\r\n                                            、麵包、水果及飲品，其實也能吃飽吃巧，同時比較不浪費食材，但就星級大飯店價位來說，\r\n                                            提供這樣的早餐品項，心情難免會覺得落差太大，當天請領檯人員另外幫我做外帶，\r\n                                            服務人員也很迅速完成準備，讓沒能來得及起床用餐的家人，吃得很很飽，下次有機會還是\r\n                                            會再次選擇入住蜂巢。'),
+('3', 'Liu', '劉小姐', '2022-04-04', 'images/blog/main-blog/m-blog-4.jpg', '服務人員,套房', '再也不來這住了', '這次住8樓，遇到一些問題，住的不是很開心\r\n                                            <br>1.晚上10點多，聽到別間傳來的洗澡水聲和管線的水聲，超級超級大聲的（已蓋過電視聲音），也持續一小時\r\n                                            <br>2.超大水聲之後，廁所開始出現很濃的霉味\r\n                                            <br>3.隔音很差，早上6點多開始，有說話聲和電視的聲音，持續2小時\r\n                                            <br> 4.停車場已經有登錄車號，卻還是不能自由進出，需要按對講機和保全通話\r\n                                            <br>因為住宿經驗很差，回來查看評價，才發現這都是很多人會遇到的相同問題，然後官方都回答，10-12樓有新客房，那為何一開始就不拿出來，等留下差評了才這樣回覆，感覺很沒誠意'),
+('4', 'Fang', '方小姐', '2022-05-08', 'images/blog/main-blog/m-blog-3.jpg', '食物,服務人員', '這家飯店的婚禮佈置', '蜂巢花園宴會廳是一間精緻西式風格又偏有點夢幻少女心的布景，沿途可見鮮花、\r\n                                            氣球、愛心、熊熊，雖然有些無法拍照(藝術品)有些可惜，不過也能讓夢幻感持續湧現。\r\n                                            基本上餐廳、會議廳空間都不是非常的寬敞，相對狹小，很容易造成壅擠的情況\r\n                                            ，假設是大型企業尾牙可能需要多考慮這點的不便性。\r\n                                            餐點魚類料理非常不滿意，很腥，感覺不是很新鮮，其餘海鮮還可以接受。\r\n                                            喜歡抹茶甜點蛋糕，濃郁且不會太甜，令人開心。整體上服務態度良好，\r\n                                            不會有多餘的親切以及不舒服的體驗，交通就在台中高鐵附近，位置方便。');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `details`
+--
+
+CREATE TABLE `details` (
+  `id` varchar(10) NOT NULL,
+  `account` varchar(20) NOT NULL,
+  `main` varchar(10) NOT NULL,
+  `name` varchar(10) NOT NULL,
+  `content` varchar(20) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 傾印資料表的資料 `details`
+--
+
+INSERT INTO `details` (`id`, `account`, `main`, `name`, `content`, `time`) VALUES
+('1', 'LinYun', '1', '林昀佑', '這個人好智障', '2022-05-15 13:13:03'),
+('2', 'guest', '1', '匿名', '服務人員真辛苦', '2022-05-05 00:20:10');
 
 -- --------------------------------------------------------
 
@@ -212,7 +236,13 @@ ALTER TABLE `cart`
 --
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `account_id` (`account_id`);
+  ADD KEY `account` (`account`);
+
+--
+-- 資料表索引 `details`
+--
+ALTER TABLE `details`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- 資料表索引 `product`
@@ -247,16 +277,6 @@ ALTER TABLE `cart`
 --
 ALTER TABLE `product`
   MODIFY `pno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
-
---
--- 已傾印資料表的限制式
---
-
---
--- 資料表的限制式 `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`id`) REFERENCES `account` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
