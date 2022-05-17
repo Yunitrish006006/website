a4534b8c0abb1cf38781cc328d02a1f0f15ffbdc
Jimmy <chjimmy661@gmail.com>
2022-05-17 23:35:59 +0800

    db.php獨立出來

--- db.php
+++ db.php
@@ -1,7 +1,3 @@
 <?php
 $link = mysqli_connect("localhost", "root", "", "group_24") or die(mysqli_error());
-if ( !$link ) {
-    echo "連結錯誤代碼: ".mysqli_connect_errno()."<br>";//顯示錯誤代碼
-    echo "連結錯誤訊息: ".mysqli_connect_error()."<br>";//顯示錯誤訊息
-    exit();
-}
+?>
