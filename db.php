755590fee901f3b044fa901c01fa0d6411c40ee9
Daniel <s0961102@gm.ncue.edu.tw>
2022-06-02 22:59:33 +0800

    db不小心改到

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
     if (session_status() === PHP_SESSION_NONE) {session_start();}
-    $link = mysqli_connect("localhost", "root", "", "group_24") or die(mysqli_error());
+    $link = mysqli_connect("localhost", "root", "root123456", "group_24") or die(mysqli_error());
 ?>
